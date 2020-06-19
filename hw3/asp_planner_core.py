37d97ccba9839d62976634f6ff02972703fd9d7a
Philip Goto <philip.goto@gmail.com>
2020-06-19 16:47:01 +0200

    Half implementation

--- hw3/asp_planner_core.py
+++ hw3/asp_planner_core.py
@@ -3,7 +3,71 @@ import planning
 
 import clingo
 
-def solve_planning_problem_using_ASP(planning_problem,t_max):
+def translate(planning_problem:PlanningProblem, trans:dict):
+
+    def recurse(exp:Expr):
+        try:
+            if isinstance(exp, Action):
+                exp.name = trans[exp.name]
+            else:
+                exp.op = trans[exp.op]
+        except KeyError:
+            pass
+        
+        for arg in exp.args:
+            recurse(arg)
+
+
+    for initial in planning_problem.initial:
+        recurse(initial)
+    
+    for action in planning_problem.actions:
+        recurse(action)
+        for precond in action.precond:
+            recurse(precond)
+        for effect in action.effect:
+            recurse(effect)
+    
+    for goal in planning_problem.goals:
+        recurse(goal)
+
+
+def build_translation(planning_problem:PlanningProblem):
+    trans = {"~": "~"}
+
+    def recurse(exp):
+        if isinstance(exp, Action):
+            name = exp.name
+        else:
+            name = exp.op
+        
+        if name not in trans:
+            if name[0].isupper():
+                trans[name] = "_" + name.lower()
+            else:
+                trans[name] = "_" + name.upper()
+        
+        for arg in exp.args:
+            recurse(arg)
+
+
+    for initial in planning_problem.initial:
+        recurse(initial)
+    
+    for action in planning_problem.actions:
+        recurse(action)
+        for precond in action.precond:
+            recurse(precond)
+        for effect in action.effect:
+            recurse(effect)
+    
+    for goal in planning_problem.goals:
+        recurse(goal)
+
+    return trans
+
+
+def solve_planning_problem_using_ASP(planning_problem:PlanningProblem, t_max:int):
     """
     If there is a plan of length at most t_max that achieves the goals of a given planning problem,
     starting from the initial state in the planning problem, returns such a plan of minimal length.
@@ -24,4 +88,59 @@ def solve_planning_problem_using_ASP(planning_problem,t_max):
         a shortest plan for planning_problem (if some plan of length at most t_max exists),
         and None otherwise.
     """
-    return None
+
+    # Create translation and inverse translation to support clingo's capital use case
+    trans = build_translation(planning_problem)
+    trans_rev = {val: key for key, val in trans.items()}
+    translate(planning_problem, trans)
+
+    asp_code = f"#const t_max={t_max}.\n"
+
+    # Define timesteps
+    asp_code += "time(0..t_max).\n"
+
+    for initial in planning_problem.initial:
+        asp_code += f"state(0, {initial}).\n"
+
+
+    for action in planning_problem.actions:
+        preconds = ", ".join([f"state(T-1, {precond})" for precond in (action.precond)] + ["time(T)"])
+        asp_code += f"available(T, {action}) :- {preconds}.\n"
+
+        effects = ", ".join(
+            [f"not state(T, {str(effect)[1:]})" for effect in action.effect if effect.op == "~"] +
+            [f"state(T, {effect})" for effect in action.effect if effect.op != "~"])
+
+        asp_code += f"{effects} :- chosen(T, {action}).\n"
+    
+    asp_code += "state(T, E) :- state(T-1, E), time(T).\n"
+    asp_code += "1 { chosen(T, A) : available(T, A) } 1 :- time(T).\n"
+
+    for goal in planning_problem.goals:
+        #asp_code += f":- not state(t_max, {goal}).\n"
+        asp_code += f"goal({goal}).\n"
+
+    asp_code += ":- not state(t_max, G), goal(G).\n"
+
+    #asp_code += "#minimize { T, state(T, G) : goal(G), time(T) }.\n"
+
+    asp_code += "{ test(state(T, G)) : goal(G), time(T) }.\n"
+    asp_code += "#show test/1.\n"
+
+    asp_code += "#show chosen/2.\n"
+
+    print(asp_code)
+
+    control = clingo.Control()
+    control.add("base", [], asp_code)
+    control.ground([("base", [])])
+    control.configuration.solve.models = 1
+
+    plan = None
+    with control.solve(yield_=True) as handle:
+        for model in handle:
+            for item in model.symbols(shown=True):
+                print(item)
+            plan = [expr(trans_rev[str(action)]) for _, action in sorted(action.arguments for action in model.symbols(shown=True))]
+
+    return plan
